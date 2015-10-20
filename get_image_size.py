226087a1c702e52a26ad9aab81dda1ec86b4241a
Wes Turner <wes@wrd.nu>
2015-10-20 07:04:59 -0500

    REF,ENH: Image.from_file, {st_atime,mtime,ctime} because already fstat

--- get_image_size.py
+++ get_image_size.py
@@ -36,26 +36,37 @@ JPEG = types['JPEG'] = 'JPEG'
 PNG = types['PNG'] = 'PNG'
 TIFF = types['TIFF'] = 'TIFF'
 
-image_fields = ['path', 'type', 'file_size', 'width', 'height']
+image_fields = ['path', 'type', 'file_size', 'width', 'height',
+                'st_ctime', 'st_mtime', 'st_atime']
 
 
 class Image(collections.namedtuple('Image', image_fields)):
 
+    @classmethod
+    def from_file(cls, *args, **kwargs):
+        return get_image_metadata(*args, **kwargs)
+
     def to_str_row(self):
-        return ("%d\t%d\t%d\t%s\t%s" % (
+        return ("%d\t%d\t%d\t%s\t%r\t%r\t%r\t%s" % (
             self.width,
             self.height,
             self.file_size,
             self.type,
+            self.st_atime,
+            self.st_mtime,
+            self.st_ctime,
             self.path.replace('\t', '\\t'),
         ))
 
     def to_str_row_verbose(self):
-        return ("%d\t%d\t%d\t%s\t%s\t##%s" % (
+        return ("%d\t%d\t%d\t%s\t%r\t%r\t%r\t%s\t\n##%s" % (
             self.width,
             self.height,
             self.file_size,
             self.type,
+            self.st_atime,
+            self.st_mtime,
+            self.st_ctime,
             self.path.replace('\t', '\\t'),
             self))
 
@@ -72,176 +83,198 @@ def get_image_size(file_path):
     return (img.width, img.height)
 
 
-def get_image_metadata(file_path):
+def get_image_metadata(fileobj, file_path=None, size=None):
+    if hasattr(fileobj, 'fileno'):
+        return _get_image_metadata(fileobj, file_path=file_path, size=size)
+    else:
+        with open(fileobj, "rb") as input:
+            return _get_image_metadata(
+                input,
+                file_path=file_path if file_path is not None else fileobj,
+                size=size)
+
+
+def _get_image_metadata(fileobj, file_path=None, size=None):
     """
     Return an `Image` object for a given img file content - no external
     dependencies except the os and struct builtin modules
 
     Args:
-        file_path (str): path to an image file
+        fileobj (str): path to an image file
 
     Returns:
         Image: (path, type, file_size, width, height)
     """
-    size = os.path.getsize(file_path)
+    fstat = None
+    if size is None:
+        fstat = os.fstat(fileobj.fileno())
+        size = fstat.st_size
+    if fstat is not None:
+        st_ctime = fstat.st_ctime
+        st_mtime = fstat.st_mtime
+        st_atime = fstat.st_atime
+    else:
+        st_ctime, st_mtime, st_atime = None, None, None
 
     # be explicit with open arguments - we need binary mode
-    with open(file_path, "rb") as input:
-        height = -1
-        width = -1
-        data = input.read(26)
-        msg = " raised while trying to decode as JPEG."
+    height = -1
+    width = -1
+    data = fileobj.read(26)
+    msg = " raised while trying to decode as JPEG."
 
-        if (size >= 10) and data[:6] in ('GIF87a', 'GIF89a'):
-            # GIFs
-            imgtype = GIF
-            w, h = struct.unpack("<HH", data[6:10])
+    if (size >= 10) and data[:6] in ('GIF87a', 'GIF89a'):
+        # GIFs
+        imgtype = GIF
+        w, h = struct.unpack("<HH", data[6:10])
+        width = int(w)
+        height = int(h)
+    elif ((size >= 24) and data.startswith('\211PNG\r\n\032\n')
+            and (data[12:16] == 'IHDR')):
+        # PNGs
+        imgtype = PNG
+        w, h = struct.unpack(">LL", data[16:24])
+        width = int(w)
+        height = int(h)
+    elif (size >= 16) and data.startswith('\211PNG\r\n\032\n'):
+        # older PNGs
+        imgtype = PNG
+        w, h = struct.unpack(">LL", data[8:16])
+        width = int(w)
+        height = int(h)
+    elif (size >= 2) and data.startswith('\377\330'):
+        # JPEG
+        imgtype = JPEG
+        fileobj.seek(0)
+        fileobj.read(2)
+        b = fileobj.read(1)
+        try:
+            while (b and ord(b) != 0xDA):
+                while (ord(b) != 0xFF):
+                    b = fileobj.read(1)
+                while (ord(b) == 0xFF):
+                    b = fileobj.read(1)
+                if (ord(b) >= 0xC0 and ord(b) <= 0xC3):
+                    fileobj.read(3)
+                    h, w = struct.unpack(">HH", fileobj.read(4))
+                    break
+                else:
+                    fileobj.read(
+                        int(struct.unpack(">H", fileobj.read(2))[0]) - 2)
+                b = fileobj.read(1)
             width = int(w)
             height = int(h)
-        elif ((size >= 24) and data.startswith('\211PNG\r\n\032\n')
-              and (data[12:16] == 'IHDR')):
-            # PNGs
-            imgtype = PNG
-            w, h = struct.unpack(">LL", data[16:24])
+        except struct.error:
+            raise UnknownImageFormat("StructError" + msg)
+        except ValueError:
+            raise UnknownImageFormat("ValueError" + msg)
+        except Exception as e:
+            raise UnknownImageFormat(e.__class__.__name__ + msg)
+    elif (size >= 26) and data.startswith('BM'):
+        # BMP
+        imgtype = 'BMP'
+        headersize = struct.unpack("<I", data[14:18])[0]
+        if headersize == 12:
+            w, h = struct.unpack("<HH", data[18:22])
             width = int(w)
             height = int(h)
-        elif (size >= 16) and data.startswith('\211PNG\r\n\032\n'):
-            # older PNGs
-            imgtype = PNG
-            w, h = struct.unpack(">LL", data[8:16])
+        elif headersize >= 40:
+            w, h = struct.unpack("<ii", data[18:26])
             width = int(w)
-            height = int(h)
-        elif (size >= 2) and data.startswith('\377\330'):
-            # JPEG
-            imgtype = JPEG
-            input.seek(0)
-            input.read(2)
-            b = input.read(1)
-            try:
-                while (b and ord(b) != 0xDA):
-                    while (ord(b) != 0xFF):
-                        b = input.read(1)
-                    while (ord(b) == 0xFF):
-                        b = input.read(1)
-                    if (ord(b) >= 0xC0 and ord(b) <= 0xC3):
-                        input.read(3)
-                        h, w = struct.unpack(">HH", input.read(4))
-                        break
-                    else:
-                        input.read(
-                            int(struct.unpack(">H", input.read(2))[0]) - 2)
-                    b = input.read(1)
-                width = int(w)
-                height = int(h)
-            except struct.error:
-                raise UnknownImageFormat("StructError" + msg)
-            except ValueError:
-                raise UnknownImageFormat("ValueError" + msg)
-            except Exception as e:
-                raise UnknownImageFormat(e.__class__.__name__ + msg)
-        elif (size >= 26) and data.startswith('BM'):
-            # BMP
-            imgtype = 'BMP'
-            headersize = struct.unpack("<I", data[14:18])[0]
-            if headersize == 12:
-                w, h = struct.unpack("<HH", data[18:22])
-                width = int(w)
-                height = int(h)
-            elif headersize >= 40:
-                w, h = struct.unpack("<ii", data[18:26])
-                width = int(w)
-                # as h is negative when stored upside down
-                height = abs(int(h))
-            else:
-                raise UnknownImageFormat(
-                    "Unkown DIB header size:" +
-                    str(headersize))
-        elif (size >= 8) and data[:4] in ("II\052\000", "MM\000\052"):
-            # Standard TIFF, big- or little-endian
-            # BigTIFF and other different but TIFF-like formats are not
-            # supported currently
-            imgtype = TIFF
-            byteOrder = data[:2]
-            boChar = ">" if byteOrder == "MM" else "<"
-            # maps TIFF type id to size (in bytes)
-            # and python format char for struct
-            tiffTypes = {
-                1: (1, boChar + "B"),  # BYTE
-                2: (1, boChar + "c"),  # ASCII
-                3: (2, boChar + "H"),  # SHORT
-                4: (4, boChar + "L"),  # LONG
-                5: (8, boChar + "LL"),  # RATIONAL
-                6: (1, boChar + "b"),  # SBYTE
-                7: (1, boChar + "c"),  # UNDEFINED
-                8: (2, boChar + "h"),  # SSHORT
-                9: (4, boChar + "l"),  # SLONG
-                10: (8, boChar + "ll"),  # SRATIONAL
-                11: (4, boChar + "f"),  # FLOAT
-                12: (8, boChar + "d")   # DOUBLE
-            }
-            ifdOffset = struct.unpack(boChar + "L", data[4:8])[0]
-            try:
-                countSize = 2
-                input.seek(ifdOffset)
-                ec = input.read(countSize)
-                ifdEntryCount = struct.unpack(boChar + "H", ec)[0]
-                # 2 bytes: TagId + 2 bytes: type + 4 bytes: count of values + 4
-                # bytes: value offset
-                ifdEntrySize = 12
-                for i in range(ifdEntryCount):
-                    entryOffset = ifdOffset + countSize + i * ifdEntrySize
-                    input.seek(entryOffset)
-                    tag = input.read(2)
-                    tag = struct.unpack(boChar + "H", tag)[0]
-                    if(tag == 256 or tag == 257):
-                        # if type indicates that value fits into 4 bytes, value
-                        # offset is not an offset but value itself
-                        type = input.read(2)
-                        type = struct.unpack(boChar + "H", type)[0]
-                        if type not in tiffTypes:
-                            raise UnknownImageFormat(
-                                "Unkown TIFF field type:" +
-                                str(type))
-                        typeSize = tiffTypes[type][0]
-                        typeChar = tiffTypes[type][1]
-                        input.seek(entryOffset + 8)
-                        value = input.read(typeSize)
-                        value = int(struct.unpack(typeChar, value)[0])
-                        if tag == 256:
-                            width = value
-                        else:
-                            height = value
-                    if width > -1 and height > -1:
-                        break
-            except Exception as e:
-                raise UnknownImageFormat(str(e))
-        elif size >= 2:
-                # see http://en.wikipedia.org/wiki/ICO_(file_format)
-            imgtype = 'ICO'
-            input.seek(0)
-            reserved = input.read(2)
-            if 0 != struct.unpack("<H", reserved)[0]:
-                raise UnknownImageFormat(FILE_UNKNOWN)
-            format = input.read(2)
-            assert 1 == struct.unpack("<H", format)[0]
-            num = input.read(2)
-            num = struct.unpack("<H", num)[0]
-            if num > 1:
-                import warnings
-                warnings.warn("ICO File contains more than one image")
-            # http://msdn.microsoft.com/en-us/library/ms997538.aspx
-            w = input.read(1)
-            h = input.read(1)
-            width = ord(w)
-            height = ord(h)
+            # as h is negative when stored upside down
+            height = abs(int(h))
         else:
+            raise UnknownImageFormat(
+                "Unkown DIB header size:" +
+                str(headersize))
+    elif (size >= 8) and data[:4] in ("II\052\000", "MM\000\052"):
+        # Standard TIFF, big- or little-endian
+        # BigTIFF and other different but TIFF-like formats are not
+        # supported currently
+        imgtype = TIFF
+        byteOrder = data[:2]
+        boChar = ">" if byteOrder == "MM" else "<"
+        # maps TIFF type id to size (in bytes)
+        # and python format char for struct
+        tiffTypes = {
+            1: (1, boChar + "B"),  # BYTE
+            2: (1, boChar + "c"),  # ASCII
+            3: (2, boChar + "H"),  # SHORT
+            4: (4, boChar + "L"),  # LONG
+            5: (8, boChar + "LL"),  # RATIONAL
+            6: (1, boChar + "b"),  # SBYTE
+            7: (1, boChar + "c"),  # UNDEFINED
+            8: (2, boChar + "h"),  # SSHORT
+            9: (4, boChar + "l"),  # SLONG
+            10: (8, boChar + "ll"),  # SRATIONAL
+            11: (4, boChar + "f"),  # FLOAT
+            12: (8, boChar + "d")   # DOUBLE
+        }
+        ifdOffset = struct.unpack(boChar + "L", data[4:8])[0]
+        try:
+            countSize = 2
+            fileobj.seek(ifdOffset)
+            ec = fileobj.read(countSize)
+            ifdEntryCount = struct.unpack(boChar + "H", ec)[0]
+            # 2 bytes: TagId + 2 bytes: type + 4 bytes: count of values + 4
+            # bytes: value offset
+            ifdEntrySize = 12
+            for i in range(ifdEntryCount):
+                entryOffset = ifdOffset + countSize + i * ifdEntrySize
+                fileobj.seek(entryOffset)
+                tag = fileobj.read(2)
+                tag = struct.unpack(boChar + "H", tag)[0]
+                if(tag == 256 or tag == 257):
+                    # if type indicates that value fits into 4 bytes, value
+                    # offset is not an offset but value itself
+                    type = fileobj.read(2)
+                    type = struct.unpack(boChar + "H", type)[0]
+                    if type not in tiffTypes:
+                        raise UnknownImageFormat(
+                            "Unkown TIFF field type:" +
+                            str(type))
+                    typeSize = tiffTypes[type][0]
+                    typeChar = tiffTypes[type][1]
+                    fileobj.seek(entryOffset + 8)
+                    value = fileobj.read(typeSize)
+                    value = int(struct.unpack(typeChar, value)[0])
+                    if tag == 256:
+                        width = value
+                    else:
+                        height = value
+                if width > -1 and height > -1:
+                    break
+        except Exception as e:
+            raise UnknownImageFormat(str(e))
+    elif size >= 2:
+            # see http://en.wikipedia.org/wiki/ICO_(file_format)
+        imgtype = 'ICO'
+        fileobj.seek(0)
+        reserved = fileobj.read(2)
+        if 0 != struct.unpack("<H", reserved)[0]:
             raise UnknownImageFormat(FILE_UNKNOWN)
+        format = fileobj.read(2)
+        assert 1 == struct.unpack("<H", format)[0]
+        num = fileobj.read(2)
+        num = struct.unpack("<H", num)[0]
+        if num > 1:
+            import warnings
+            warnings.warn("ICO File contains more than one image")
+        # http://msdn.microsoft.com/en-us/library/ms997538.aspx
+        w = fileobj.read(1)
+        h = fileobj.read(1)
+        width = ord(w)
+        height = ord(h)
+    else:
+        raise UnknownImageFormat(FILE_UNKNOWN)
 
     return Image(path=file_path,
                  type=imgtype,
                  file_size=size,
                  width=width,
-                 height=height)
+                 height=height,
+                 st_ctime=st_ctime,
+                 st_mtime=st_mtime,
+                 st_atime=st_atime)
 
 
 import unittest
@@ -268,10 +301,38 @@ class Test_get_image_size(unittest.TestCase):
         self.assertEqual(output.type, img['type'])
         self.assertEqual(output.file_size, img['file_size'])
         for field in image_fields:
-            self.assertEqual(getattr(output, field), img[field])
+            if field not in ['st_ctime', 'st_mtime', 'st_atime']:
+                self.assertEqual(getattr(output, field), img[field])
+
+    def test_get_image_metadata__Image__from_file(self):
+        img = self.data[0]
+        output = Image.from_file(img['path'])
+        self.assertTrue(output)
+        self.assertEqual(output.path, img['path'])
+        self.assertEqual(output.width, img['width'])
+        self.assertEqual(output.height, img['height'])
+        self.assertEqual(output.type, img['type'])
+        self.assertEqual(output.file_size, img['file_size'])
+        for field in image_fields:
+            if field not in ['st_ctime', 'st_mtime', 'st_atime']:
+                self.assertEqual(getattr(output, field), img[field])
+
+    def test_get_image_metadata__fileobj(self):
+        img = self.data[0]
+        with open(img['path'], 'rb') as fileobj:
+            output = get_image_metadata(fileobj, file_path=img['path'])
+            self.assertTrue(output)
+            self.assertEqual(output.path, img['path'])
+            self.assertEqual(output.width, img['width'])
+            self.assertEqual(output.height, img['height'])
+            self.assertEqual(output.type, img['type'])
+            self.assertEqual(output.file_size, img['file_size'])
+            for field in image_fields:
+                if field not in ['st_ctime', 'st_mtime', 'st_atime']:
+                    self.assertEqual(getattr(output, field), img[field])
 
     def test_get_image_metadata__ENOENT_OSError(self):
-        with self.assertRaises(OSError):
+        with self.assertRaises((IOError, OSError)):
             get_image_metadata('THIS_DOES_NOT_EXIST')
 
     def test_get_image_metadata__not_an_image_UnknownImageFormat(self):
